1b8ecb2334e75504d1708826fd52a08aa60f4f57
Joao <joaogabrield@hotmail.com>
2025-08-13 00:04:41 -0300

    db e endpoints de criar tipos de exercicios

---
+++ src/my_api/bd_exercicio.clj
@@ -0,0 +1,71 @@
+(ns my-api.bd-exercicio
+  (:require [clojure.string :as str]
+            [datomic.api :as d]))
+
+(def exercicio-schema
+  [;; Identificador único do exercício
+   {:db/ident       :exercicio/id
+    :db/valueType   :db.type/uuid
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity
+    :db/doc         "ID único do exercício"}
+
+   ;; Nome de exibição do exercício
+   {:db/ident       :exercicio/nome
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Nome de exibição do exercício"}
+
+   ;; Nome interno único (slug/keyword)
+   {:db/ident       :exercicio/nome-interno
+    :db/valueType   :db.type/keyword
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity
+    :db/doc         "Nome interno único do exercício"}
+
+   ;; Categoria (ex.: perna, peito, costas)
+   {:db/ident       :exercicio/categoria
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Categoria do exercício"}])
+
+(def db-uri "datomic:dev://localhost:4334/meu-banco")
+(def conn (d/connect db-uri))
+
+(defn ensure-exercicio-schema []
+  @(d/transact conn exercicio-schema))
+
+(defn- gerar-id-exercicio []
+  (java.util.UUID/randomUUID))
+
+(defn- coerce-nome-interno [value]
+  (if (string? value)
+    (-> value str/trim str/lower-case (str/replace #"\s+" "-") keyword)
+    (throw (ex-info "nome-interno inválido (esperado string)" {:valor value}))))
+
+(defn inserir-exercicio!
+  "Insere um exercício. Aceita :nome (string) e :categoria (string). Retorna dados essenciais cadastrados."
+  [{:keys [nome categoria]}]
+  (let [nome-interno-kw (coerce-nome-interno nome)
+        id (gerar-id-exercicio)
+        tx {:exercicio/id id
+            :exercicio/nome nome
+            :exercicio/nome-interno nome-interno-kw
+            :exercicio/categoria categoria}]
+    @(d/transact conn [tx])
+    {:id id :nome nome :nome-interno nome-interno-kw :categoria categoria}))
+
+(defn listar-exercicios
+  "Retorna vetor de mapas contendo :nome e :nome-interno para todos os exercícios."
+  []
+  (->> (d/q '[:find ?nome ?nome-interno
+              :where
+              [?e :exercicio/nome ?nome]
+              [?e :exercicio/nome-interno ?nome-interno]]
+            (d/db conn))
+       (map (fn [[nome nome-interno]] 
+              {:nome (str/capitalize nome)
+               :nome-interno (name nome-interno)}))
+       (sort-by :nome)
+       (vec)))
+
--- src/my_api/service.clj
+++ src/my_api/service.clj
@@ -2,16 +2,25 @@
   (:require [io.pedestal.http :as http]
             [my-api.bd-user :as db]
             [my-api.bd-treino :as treino-db]
-            [io.pedestal.http.body-params :as body-params]))
+            [my-api.bd-exercicio :as exercicio-db]
+            [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.cors :as cors]
+            [clojure.data.json :as json]))
 
 (defn user-internal->out [[id _ name age]]
 {:id id :name name :age age})
 
 (db/ensure-schema)
 (treino-db/ensure-treino-schema)
+(exercicio-db/ensure-exercicio-schema)
+
+(def cors-interceptor
+  (cors/allow-origin {:allowed-origins (constantly true)
+                      :creds true
+                      :methods #{:get :put :post :delete :options}}))
 
 (def common-interceptors
-  [(body-params/body-params)])
+  [cors-interceptor (body-params/body-params)])
 
 (defn home-page [_request]
   {:status 200 :body "Hello, Pedestal!"})
@@ -31,9 +40,23 @@
 
 ;; Handlers para treinos
 (defn create-treino-handler [request]
-  (let [treino (:edn-params request)]
+  (let [treino (or (:edn-params request) (:json-params request))]
     (treino-db/inserir-treino! treino)
     {:status 200 :body (str "Treino cadastrado: " treino)}))
+
+;; Handler para cadastro de exercício
+(defn create-exercicio-handler [request]
+  (let [exercicio (or (:edn-params request) (:json-params request))]
+    (print "exercicio: " request)
+    (exercicio-db/inserir-exercicio! exercicio)
+    {:status 200 :body (str "Exercício cadastrado: " exercicio)}))
+
+;; Handler para listar exercícios
+(defn listar-exercicios-handler [_request]
+  (let [items (exercicio-db/listar-exercicios)]
+    {:status 200 
+     :headers {"Content-Type" "application/json"}
+     :body (json/write-str items)}))
 
 ;; Novo handler para exibir o histórico de um exercício
 (defn exibir-exercicio-handler [request]
@@ -67,6 +90,14 @@
      :post (conj common-interceptors create-treino-handler)
      :route-name :create-treino]
     
+    ["/create-exercicio"
+     :post (conj common-interceptors create-exercicio-handler)
+     :route-name :create-exercicio]
+
+    ["/exercicios"
+     :get (conj common-interceptors listar-exercicios-handler)
+     :route-name :listar-exercicios]
+    
     ["/exibir-exercicio/:exercicio"
      :get (conj common-interceptors exibir-exercicio-handler)
      :route-name :exibir-exercicio]
@@ -79,4 +110,5 @@
   {:env :prod
    ::http/routes routes
    ::http/type :jetty
-   ::http/port 8080})
+   ::http/port 8080
+   ::http/allowed-origins {:creds true :allowed-origins (constantly true)}})
